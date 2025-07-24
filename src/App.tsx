5fb9f6cac3a1014b714449a1f5be9abf1e4114c5
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-07-24 19:45:26 +0100

    Premium course added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import One from './Pages/Home/One';
 import Course1 from './Pages/Cert/Course1';
+import GenomacLabs from './Pages/Home/GenomacLabs';
+import Course2 from './Pages/Cert/Course2';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<One />} />
           <Route path="/course1" element={<Course1 />} />
+          <Route path="/course2" element={<Course2 header="Certificate of Completion" courseTitle="," date="21st - 24th July 2025" />} />
+          <Route path="/genomac-labs" element={<GenomacLabs />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Pages/Home/GenomacLabs.tsx
@@ -0,0 +1,13 @@
+import Homepage from "../../Components/Homepage";
+
+
+
+const GenomacLabs = () => {
+  return (
+    <>
+        <Homepage navi="/course2" />
+    </>
+  )
+}
+
+export default GenomacLabs
